b5f689c4098c3936ab3903714cd3848e093245f6
msyme <msyme@DESKTOP-IUC8NCA>
2019-04-08 20:00:02 -0400

    Final Lab3

--- SudokuBLL/src/test/java/pkgGame/SudokuPrivateMethodsTest.java
+++ SudokuBLL/src/test/java/pkgGame/SudokuPrivateMethodsTest.java
@@ -14,7 +14,7 @@ public class SudokuPrivateMethodsTest {
 	public void FillDiagonalRegions() {
 		try {
 			Sudoku s = new Sudoku(9);
-			s.FillDiagonalRegions(); /*figure out java reflections*/
+			s.FillDiagonalRegions(); 
 			int[] expectRegion = {1,2,3,4,5,6,7,8,9};
 			int[] region0 = s.getRegion(0);
 			int[] region4 = s.getRegion(4);
@@ -30,13 +30,34 @@ public class SudokuPrivateMethodsTest {
 		}
 		
 	}
-	
+	@Test
+	public void PrintStars() {
+		Sudoku s1= null;
+		
+		int[][] puzzle = { 
+				{ 5, 3, 4, 6, 7, 8, 9, 1, 2 }, 
+				{ 6, 7, 2, 1, 9, 5, 3, 4, 8 }, 
+				{ 1, 9, 8, 3, 4, 2, 5, 6, 7 },
+				{ 8, 5, 9, 7, 6, 1, 4, 2, 3 }, 
+				{ 4, 2, 6, 8, 5, 3, 7, 9, 1 }, 
+				{ 7, 1, 3, 9, 2, 4, 8, 5, 6 },
+				{ 9, 6, 1, 5, 3, 7, 2, 8, 4 }, 
+				{ 2, 8, 7, 4, 1, 9, 6, 3, 5 }, 
+				{ 3, 4, 5, 2, 8, 6, 1, 7, 9 } };
+		try {
+			s1 = new Sudoku(puzzle);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		s1.PrintPuzzle();
+	}
 	
 	@Test
 	public void Sudoku_Test_SetRegion() {
 		try {
 			Sudoku s = new Sudoku(9);
-			s.SetRegion(1); /*how do you use reflections*/
+			s.SetRegion(1); 
 			
 			int[] expectRegion = {1,2,3,4,5,6,7,8,9};
 			int[] region = s.getRegion(1);
@@ -48,5 +69,7 @@ public class SudokuPrivateMethodsTest {
 			e.printStackTrace();
 		}
 	}
+	
+	
 	
 }
